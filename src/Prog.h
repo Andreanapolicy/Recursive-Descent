f3d9bd77a390171fd5e9fefaff64093bf11476b5
kazantsev-roman <dj.ka3anova@gmail.com>
2022-12-22 11:21:36 +0300

    реализовал Prog, заложил Var

--- src/Prog.h
+++ src/Prog.h
@@ -1,6 +1,30 @@
 #pragma once
+#include "./Common/IFunction.h"
+#include "./Common/TerminalSymbols.h"
+#include "./Common/NonTerminalSymbols.h"
+#include <iostream>
+
+// <PROG>->PROG id <VAR> begin <LISTST> end
 
 bool executeProg(std::string& data)
 {
-    return true;
+    if (executableHandlers.find(non_terminal_symbols::VAR)->second(data))
+    {
+        if (data.starts_with(terminal_symbols::BEGIN))
+        {
+            data.erase(0, terminal_symbols::BEGIN.size() + 1);
+            if (executableHandlers.find(non_terminal_symbols::LISTST)->second(data))
+            {
+                if (data.starts_with(terminal_symbols::END))
+                {
+                    data.erase(0, terminal_symbols::END.size());
+                    return data.empty();
+                }
+                return false;
+            }
+            return false;
+        }
+        return false;
+    }
+    return false;
 }
--- src/Var.h
+++ src/Var.h
@@ -1,5 +1,7 @@
 #pragma once
 
+//<VAR>->VAR < IDLIST> : <TYPE>
+
 bool executeVar(std::string& data)
 {
     return true;
