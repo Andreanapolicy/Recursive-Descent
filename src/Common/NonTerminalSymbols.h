bb3570803fac2bc1d72b6b89993ea069f49c39f6
Andreanapolicy <daa.and@mail.ru>
2022-12-22 10:15:05 +0300

    Добавил нетерминальные символы

--- src/Common/NonTerminalSymbols.h
+++ src/Common/NonTerminalSymbols.h
@@ -1,2 +1,15 @@
 #pragma once
+#include <string>
 
+const std::string PROG = "PROG";
+const std::string VAR = "VAR";
+const std::string LISTST = "LISTST";
+const std::string IDLIST = "IDLIST";
+const std::string ST = "ST";
+const std::string TYPE = "TYPE";
+const std::string READ = "READ";
+const std::string WRITE = "WRITE";
+const std::string ASSIGN = "ASSIGN";
+const std::string EXP = "EXP";
+const std::string T = "T";
+const std::string F = "F";
