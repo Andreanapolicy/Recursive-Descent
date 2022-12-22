ec3f0aa4375168e02fc0ee0dece9ca70951c3aca
m3tro1d <qazwf734@gmail.com>
2022-12-22 10:41:16 +0300

    Implement non terminal functions

--- src/Common/TerminalSymbols.h
+++ src/Common/TerminalSymbols.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cctype>
 #include <string>
 
 const std::string ID = "id";
@@ -13,12 +14,39 @@ const std::string PLUS = "+";
 const std::string MINUS = "-";
 const std::string MULTIPLY = "*";
 
+bool isAlphaNumeric(std::string const& data)
+{
+	return std::find_if(
+			   data.begin(),
+			   data.end(),
+			   [](char c) { return !(std::isalnum(c) || (c == ' ')); })
+		== data.end();
+}
+
 bool executeId(std::string& data)
 {
-	return true;
+	if (data.length() == 0)
+	{
+		return false;
+	}
+
+	if (!std::isalpha(data[0]))
+	{
+		return false;
+	}
+
+	return isAlphaNumeric(data);
 }
 
 bool executeNum(std::string& data)
 {
-	return true;
+	try
+	{
+		std::stoi(data);
+		return true;
+	}
+	catch (...)
+	{
+		return false;
+	}
 }
