b58a82a497ea9dd707b3b59b9667589191405430
Andreanapolicy <daa.and@mail.ru>
2022-12-22 10:21:11 +0300

    Добавил терминальные символы

---
+++ src/Common/TerminalSymbols.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+const std::string ID = "id";
+const std::string BEGIN = "begin";
+const std::string END = "end";
+const std::string INT = "int";
+const std::string FLOAT = "float";
+const std::string BOOL = "bool";
+const std::string STRING = "string";
+const std::string ASSIGMENT = ":=";
+const std::string PLUS = "+";
+const std::string MINUS = "-";
+const std::string MULTIPLY = "*";
