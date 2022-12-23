73831d2c40df823d1ddcffc4641a2c3179636c19
kazantsev-roman <dj.ka3anova@gmail.com>
2022-12-23 17:33:34 +0300

    реализовал Var, добавил недостающие терминальные символы и добавил необходимые handlers в мапу

--- src/Common/IFunction.cpp
+++ src/Common/IFunction.cpp
@@ -3,12 +3,16 @@
 #include "../Var.h"
 #include "../Prog.h"
 #include "../Listst.h"
+#include "../Type.h"
+#include "../Idlist.h"
 #include <map>
 
 std::map<std::string, Handler> executableHandlers = {
+		{non_terminal_symbols::PROG, executeProg},
         {non_terminal_symbols::VAR, executeVar},
-        {non_terminal_symbols::PROG, executeProg},
-        {non_terminal_symbols::LISTST, executeListst}
+        {non_terminal_symbols::LISTST, executeListst},
+		{non_terminal_symbols::TYPE, executeType},
+		{non_terminal_symbols::IDLIST, executeIdlist}
 };
 
 Handler getHandler(const std::string& symbols)
--- src/Var.h
+++ src/Var.h
@@ -1,9 +1,31 @@
 #pragma once
-#include <string>
+#include "./Common/IFunction.h"
+#include "./Common/TerminalSymbols.h"
+#include "./Common/NonTerminalSymbols.h"
 
-//<VAR>->VAR < IDLIST> : <TYPE>
+//<VAR>->VAR <IDLIST> : <TYPE>;
 
 bool executeVar(std::string& data)
 {
-    return true;
+	if (data.starts_with(terminal_symbols::VAR))
+	{
+		// "VAR "
+		data.erase(0, terminal_symbols::VAR.size() + 1);
+		if (getHandler(non_terminal_symbols::IDLIST)(data))
+		{
+			if (data.starts_with(terminal_symbols::COLON))
+			{
+				// ": "
+				data.erase(0, terminal_symbols::COLON.size() + 1);
+				if (getHandler(non_terminal_symbols::TYPE)(data))
+				{
+					// "; "
+					data.erase(0, terminal_symbols::SEMICOLON.size() + 1);
+					return true;
+				}
+			}
+		}
+	}
+
+	return false;
 }
