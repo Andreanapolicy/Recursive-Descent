454975c1cd7fccaeb110a9564b89981d414d2e65
kazantsev-roman <dj.ka3anova@gmail.com>
2022-12-23 17:50:48 +0300

    добавил поясняющие комментарии с функция удаления

--- src/Type.h
+++ src/Type.h
@@ -10,24 +10,28 @@ bool executeType(std::string& data)
 {
     if (data.starts_with(terminal_symbols::INT))
 	{
+		// "int"
 		data.erase(0, terminal_symbols::INT.size());
 		return true;
 	}
 
 	if (data.starts_with(terminal_symbols::FLOAT))
 	{
+		// "float"
 		data.erase(0, terminal_symbols::FLOAT.size());
 		return true;
 	}
 
 	if (data.starts_with(terminal_symbols::BOOL))
 	{
+		// "bool"
 		data.erase(0, terminal_symbols::BOOL.size());
 		return true;
 	}
 
 	if (data.starts_with(terminal_symbols::STRING))
 	{
+		// "string"
 		data.erase(0, terminal_symbols::STRING.size());
 		return true;
 	}
