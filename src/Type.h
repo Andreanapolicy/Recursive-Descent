a603c2de1db4e1174f384f55cd01529f9c559f09
kazantsev-roman <dj.ka3anova@gmail.com>
2022-12-23 17:33:48 +0300

    реализовал Type

--- src/Type.h
+++ src/Type.h
@@ -1,7 +1,36 @@
 #pragma once
+#include "./Common/IFunction.h"
+#include "./Common/TerminalSymbols.h"
+#include "./Common/NonTerminalSymbols.h"
 #include <string>
+
+// int|float|bool|string
 
 bool executeType(std::string& data)
 {
-    return true;
+    if (data.starts_with(terminal_symbols::INT))
+	{
+		data.erase(0, terminal_symbols::INT.size());
+		return true;
+	}
+
+	if (data.starts_with(terminal_symbols::FLOAT))
+	{
+		data.erase(0, terminal_symbols::FLOAT.size());
+		return true;
+	}
+
+	if (data.starts_with(terminal_symbols::BOOL))
+	{
+		data.erase(0, terminal_symbols::BOOL.size());
+		return true;
+	}
+
+	if (data.starts_with(terminal_symbols::STRING))
+	{
+		data.erase(0, terminal_symbols::STRING.size());
+		return true;
+	}
+
+	return false;
 }
