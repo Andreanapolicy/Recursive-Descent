9b2456d12ef390a12734e18ce10fe61fece9d2a6
Andreanapolicy <daa.and@mail.ru>
2022-12-25 14:14:11 +0300

    Добавил реализацию read(то же самое, что и write, только замена терминального символа)

--- src/Read.h
+++ src/Read.h
@@ -1,7 +1,44 @@
 #pragma once
+#include "Common/TerminalSymbols.h"
 #include <string>
+
+//<READ> -> READ (<IDLIST>);
 
 bool executeRead(std::string& data)
 {
-	return true;
+	removeBlanks(data);
+
+	if (!data.starts_with(terminal_symbols::READ))
+	{
+		return false;
+	}
+	data.erase(0, terminal_symbols::READ.size());
+	removeBlanks(data);
+
+	if (!data.starts_with(terminal_symbols::OPENING_BRACE))
+	{
+		return false;
+	}
+	data.erase(0, terminal_symbols::OPENING_BRACE.size());
+
+	auto const bracePos = data.find(terminal_symbols::CLOSING_BRACE);
+	if (bracePos == std::string::npos)
+	{
+		return false;
+	}
+
+	auto idList = data.substr(0, bracePos);
+	if (!getHandler(non_terminal_symbols::IDLIST))
+	{
+		return false;
+	}
+
+	if (!data.starts_with(terminal_symbols::SEMICOLON))
+	{
+		return false;
+	}
+
+	removeBlanks(data);
+
+	return data.empty();
 }
