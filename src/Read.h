0dcefe5c58c930e3d5ab8578cef93155ad96212a
Andreanapolicy <daa.and@mail.ru>
2022-12-25 14:34:59 +0300

    Добавил выброс исключений в read

--- src/Read.h
+++ src/Read.h
@@ -10,32 +10,32 @@ bool executeRead(std::string& data)
 
 	if (!data.starts_with(terminal_symbols::READ))
 	{
-		return false;
+		throw std::runtime_error("Cannot find `READ` in read");
 	}
 	data.erase(0, terminal_symbols::READ.size());
 	removeBlanks(data);
 
 	if (!data.starts_with(terminal_symbols::OPENING_BRACE))
 	{
-		return false;
+		throw std::runtime_error("Cannot find opening brace in read");
 	}
 	data.erase(0, terminal_symbols::OPENING_BRACE.size());
 
 	auto const bracePos = data.find(terminal_symbols::CLOSING_BRACE);
 	if (bracePos == std::string::npos)
 	{
-		return false;
+		throw std::runtime_error("Cannot find closing brace in read");
 	}
 
 	auto idList = data.substr(0, bracePos);
 	if (!getHandler(non_terminal_symbols::IDLIST)(idList))
 	{
-		return false;
+		throw std::runtime_error("Id list is not correct in read");
 	}
 
 	if (!data.starts_with(terminal_symbols::SEMICOLON))
 	{
-		return false;
+		throw std::runtime_error("Cannot find semicolon in read");
 	}
 
 	removeBlanks(data);
